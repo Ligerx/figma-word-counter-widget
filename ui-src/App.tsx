52df0da8d7dc27e5f4976d85a8ea507bdf9d4793
Ligerx <6577974+Ligerx@users.noreply.github.com>
2023-02-05 11:09:15 -0800

    add plugin UI button css

--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <button
+        className="button button--primary"
         onClick={() => {
           parent?.postMessage?.(
             { pluginMessage: "confirm-layer-selection" },
